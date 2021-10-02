6b3c42a71667bd99afd4359fa6cff3f6fd111df9
Joe Kim <joekim6a@gmail.com>
2021-10-02 16:37:39 -0700

    remove duplicates from sorted array

--- practice.js
+++ practice.js
@@ -1,17 +1,35 @@
-// Write a function to find the longest common prefix string amongst an array of strings.
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
 
-// If there is no common prefix, return an empty string "".
+// Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
 
-var longestCommonPrefix = function(strs) {
-    if (strs.length === 0) return '';
+// Return k after placing the final result in the first k slots of nums.
 
-    let prefix = strs[0];
+// Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
 
-    for (let i = 1; i <= strs.length - 1; i++) {
-        while (strs[i].indexOf(prefix) !== 0) {
-            prefix = prefix.slice(0, prefix.length - 1)
+// Custom Judge:
+
+// The judge will test your solution with the following code:
+
+// int[] nums = [...]; // Input array
+// int[] expectedNums = [...]; // The expected answer with correct length
+
+// int k = removeDuplicates(nums); // Calls your implementation
+
+// assert k == expectedNums.length;
+// for (int i = 0; i < k; i++) {
+//     assert nums[i] == expectedNums[i];
+// }
+// If all assertions pass, then your solution will be accepted.
+
+var removeDuplicates = function(nums) {
+    let index = 1;
+
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] !== nums[i + 1]) {
+            nums[index] = nums[i + 1];
+            index++;
         }
     }
 
-    return prefix;
+    return index;
 };
