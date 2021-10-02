c8c0d2c57880634d2f0a14108fe76ab4bfd9e42e
Joe Kim <joekim6a@gmail.com>
2021-10-01 17:04:08 -0700

    roman to integer

--- practice.js
+++ practice.js
@@ -1,30 +1,66 @@
-// Merge Sorted Arrays
-// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
 
-// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+// For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
 
-// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+// Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
 
-var merge = function(nums1, m, nums2, n) {
-    m--;
-    n--;
+// I can be placed before V (5) and X (10) to make 4 and 9.
+// X can be placed before L (50) and C (100) to make 40 and 90.
+// C can be placed before D (500) and M (1000) to make 400 and 900.
+// Given a roman numeral, convert it to an integer.
 
-    let i = nums1.length - 1;
+var romanToInt = function(s) {
+    let num = 0;
 
-    while (i >= 0) {
-        if (m < 0) {
-            nums1[i] = nums2[n]
-            n--;
-        } else if (n < 0) {
-            nums1[i] = nums1[m]
-            m--;
-        } else if (nums1[m] > nums2[n]) {
-            nums1[i] = nums1[m]
-            m--;
-        } else {
-            nums1[i] = nums2[n];
-            n--;
+    for (let i = 0; i <= s.length - 1; i++) {
+        if (s[i] === 'I') {
+            if (s[i + 1] === 'V' || s[i + 1] === 'X') {
+                num -= 1;
+            } else {
+                num += 1;
+            }
         }
-        i--;
+
+        if (s[i] === 'X') {
+            if (s[i + 1] === 'L' || s[i + 1] === 'C') {
+                num -= 10;
+            } else {
+                num += 10;
+            }
+        }
+
+        if (s[i] === 'C') {
+            if (s[i + 1] === 'D' || s[i + 1] === 'M') {
+                num -= 100;
+            } else {
+                num += 100;
+            }
+        }
+
+        if (s[i] === 'V') {
+            num += 5;
+        }
+
+        if (s[i] === 'L') {
+            num += 50;
+        }
+
+        if (s[i] === 'D') {
+            num += 500;
+        }
+
+        if (s[i] === 'M') {
+            num += 1000;
+        }
     }
+
+    return num;
 };
